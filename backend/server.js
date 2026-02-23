b6579cafd606f79689c77044a984c1d2320d8df5
MrDavil125 <lenovocomputer125@gmail.com>
2026-02-23 13:09:35 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("API Running...");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server Started");
+});
